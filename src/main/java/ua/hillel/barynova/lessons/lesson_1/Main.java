3b4fd2d881d9fa5afdfdae200449680a3318e42c
Sofia Barynova <sofiabarynova@gmail.com>
2025-03-12 19:35:37 +0200

    Первый коммит

---
+++ src/main/java/ua/hillel/barynova/lessons/lesson_1/Main.java
@@ -0,0 +1,8 @@
+package ua.hillel.barynova.lessons.lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello Java");
+    }
+}
---
+++ src/main/java/ua/hillel/barynova/lessons/lesson_2/Main.java
@@ -0,0 +1,21 @@
+package ua.hillel.barynova.lessons.lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double latitudeEiffelTower = 48.8584;
+        double longitudeEiffelTower = 2.2945;
+
+        double latitudeGreatWall = 40.4319;
+        double longitudeGreatWall = 116.5704;
+
+        char degreeSymbol = '\u00B0';
+
+        System.out.println("Eiffel Tower (Paris, France)" + "\n" + latitudeEiffelTower + degreeSymbol + "N" + "\n" + longitudeEiffelTower + degreeSymbol + "E" + "\n");
+        System.out.println("Great Wall of China (Badaling, China)" + "\n" + latitudeGreatWall + degreeSymbol + "N" + "\n" + longitudeGreatWall + degreeSymbol + "E");
+
+
+
+
+    }
+}
