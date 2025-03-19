4d48bbcdee54722a646511fa626d813c0f0dda29
Sofia Barynova <sofiabarynova@gmail.com>
2025-03-19 18:29:32 +0200

    Commit lesson 6

--- src/main/java/ua/hillel/barynova/lessons/lesson_6/GuessNumber.java
+++ src/main/java/ua/hillel/barynova/lessons/lesson_6/GuessNumber.java
@@ -1,7 +1,45 @@
 package ua.hillel.barynova.lessons.lesson_6;
 
+import java.util.Scanner;
+
 public class GuessNumber {
     public static void main(String[] args) {
+
+       int secretNumber = (int) (Math.random() * (30 - 10 + 1) + 10);
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Guess the secret number in 3 tries!!! From 10 to 30 \n");
+
+        for (int i = 0; i < 3 ; i++) {
+            System.out.println("Please enter your secret number: ");
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("Error!!! Enter a number between 10 and 30");
+                scanner.next();
+                continue;
+            }
+
+            int userNumber = scanner.nextInt();
+
+            if (userNumber < 10 || userNumber > 30 ){
+                System.out.println("The number is out of range!!! Enter a number between 10 and 30");
+                continue;
+
+            }
+
+            if (userNumber == secretNumber) {
+                System.out.println("Congratulations! You guessed the number: " + secretNumber);
+            } else {
+                System.out.println("Not a guess! Attempts left: " + (2 - i));
+            }
+
+            if (i == 2) {
+                System.out.println("You've lost! The guessed number was: " + secretNumber);
+            }
+
+        }
+
+        scanner.close();
 
     }
 }
--- src/main/java/ua/hillel/barynova/lessons/lesson_6/ShuttleNumbers.java
+++ src/main/java/ua/hillel/barynova/lessons/lesson_6/ShuttleNumbers.java
@@ -3,5 +3,34 @@ package ua.hillel.barynova.lessons.lesson_6;
 public class ShuttleNumbers {
     public static void main(String[] args) {
 
+        int counter = 0;
+
+        for (int i = 1; ; i++) {
+            int temp = i;
+            boolean found = false;
+
+            while (temp > 0) {
+                int digit = temp % 10;
+
+                if (digit == 4 || digit == 9){
+                    found = true;
+                    break;
+                }
+                temp /= 10;
+            }
+
+            if (found) {
+                continue;
+            }
+
+            counter++;
+            System.out.println(counter + ") " + i);
+
+            if (counter == 100) {
+                break;
+            }
+
+        }
+
     }
 }
